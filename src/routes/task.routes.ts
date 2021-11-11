2b36fb6acf445b29e3c2d1a7d5429890f02df3b3
devAndre-Isaac <andreisaac344@gmail.com>
2021-11-11 20:07:47 -0300

    feat: :sparkles: add route for update

--- src/routes/task.routes.ts
+++ src/routes/task.routes.ts
@@ -7,6 +7,6 @@ const routes = Router();
 routes.post("/tasks/save", tasksController.store)
 routes.get("/tasks/read", tasksController.read)
 routes.get("/tasks/read/:id", tasksController.readById)
-
+routes.put("/tasks/update/:id", tasksController.update)
 
   export default routes
