2ee594d0dd98fce03b075cdf809e636be3fed079
devAndre-Isaac <andreisaac344@gmail.com>
2021-11-11 19:56:16 -0300

    feat: :sparkles: add get controllers

--- src/controller/tasks.ts
+++ src/controller/tasks.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { getMongoRepository } from "typeorm";
-import { Tasks } from '../entity/tasks'
+
+import { Tasks } from '../entity/tasks';
 
 
 class tasksController {
@@ -9,6 +10,17 @@ class tasksController {
       const taskToSave = repository.create(req.body)
       const task = await repository.save(taskToSave);
       return res.status(201).json(task);
+    }
+    async read(req: Request, res: Response) {
+      const repository = getMongoRepository(Tasks);
+      const read = await repository.find()
+      return res.status(200).json(read)
+    }
+    async readById(req: Request, res: Response) {
+      const repository = getMongoRepository(Tasks);
+      const { id } = req.params
+      const readById = repository.findOne(id)
+      return res.status(200).json(readById)
     }
 }
 export default new tasksController();
