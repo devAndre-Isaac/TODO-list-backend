400be8ea880872306644ab158a06981d52e72816
devAndre-Isaac <andreisaac344@gmail.com>
2021-11-11 19:57:55 -0300

    fix:

--- src/routes/task.routes.ts
+++ src/routes/task.routes.ts
@@ -6,7 +6,7 @@ const routes = Router();
 
 routes.post("/tasks/save", tasksController.store)
 routes.get("/tasks/read", tasksController.read)
-routes.get("/tasks/read", tasksController.readById)
+routes.get("/tasks/read/:id", tasksController.readById)
 
 
   export default routes
