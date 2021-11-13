4cc7932e1f565b16598333221ba937406a8f8e61
devAndre-Isaac <andreisaac344@gmail.com>
2021-11-13 14:54:14 -0300

    feat: :sparkles: add completed task router in server

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,15 @@ import cors from "cors";
 import "./database";
 
 import taskRouter from "./routes/task.routes";
+import completedTaskRouter from "./routes/tasksCompleted.routes";
+
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(taskRouter);
+app.use('/tasks',taskRouter);
+app.use('/tasks', completedTaskRouter);
 
 app.get("/", (request, response) => {
   return response.json({ message: "TODO-List - Versão 0.1" });
