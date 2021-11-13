40b6c23d68cee02f1a0772ad3b0203911ba9b73f
devAndre-Isaac <andreisaac344@gmail.com>
2021-11-13 14:54:42 -0300

    feat: :sparkles: add entity for completed_tasks

---
+++ src/entity/completedTaks.ts
@@ -0,0 +1,27 @@
+import {
+    Entity,
+    Column,
+    CreateDateColumn,
+    UpdateDateColumn,
+    ObjectIdColumn,
+    ObjectID,
+  } from "typeorm";
+  
+  @Entity("completed_tasks")
+  export class CompletedTasks {
+    @ObjectIdColumn()
+    id: ObjectID;
+  
+    @Column({nullable: true})
+    task: string
+    
+    @Column({nullable: false})
+    done: boolean
+
+    @CreateDateColumn()
+    created_at: Date;
+  
+    @UpdateDateColumn()
+    update_at: Date;
+  }
+  
