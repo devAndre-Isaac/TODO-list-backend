cea5be01ebf9dd7a6335895a2c48e19f7a5183cf
devAndre-Isaac <andreisaac344@gmail.com>
2021-11-14 16:01:47 -0300

    feat: :sparkles: add completed task controller

--- src/controller/completed.ts
+++ src/controller/completed.ts
@@ -4,20 +4,21 @@ import { CompletedTasks } from "../entity/completedTaks";
 import { Tasks } from "../entity/tasks";
 
 class completedController {
-    async mark(req: Request, res: Response) {
-        const repository = getMongoRepository(CompletedTasks);
-        const repositoryForVerify = getMongoRepository(Tasks);
-       
-        const { id } = req.params
+  async mark(req: Request, res: Response) {
+    const repositoryForVerify = getMongoRepository(Tasks);
 
-        const verifTask = await repositoryForVerify.findOne(id)
+    const { id } = req.params;
+    const { done } = req.body
 
-        const taskToSave = repository.create(req.body)
-        
-        const task = await repository.save(taskToSave);
-        
-        return res.status(201).json(task);
-      }
+    const verifTask = await repositoryForVerify.findOne(id);
+    
+    const markedTask = {...verifTask, done: done};
+
+    const taskToSave = repositoryForVerify.create(markedTask as any);
+    const task = await repositoryForVerify.save(taskToSave);
+
+    return res.status(201).json(task);
+  }
 }
 
 export default new completedController();
