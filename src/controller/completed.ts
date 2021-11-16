0eaeb26c4f36b5f1a1aad869b7846f6faf82562a
devAndre-Isaac <andreisaac344@gmail.com>
2021-11-16 19:32:43 -0300

    feat: :sparkles: add readAllCompletedTasks controller

--- src/controller/completed.ts
+++ src/controller/completed.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+
 import { getMongoRepository } from "typeorm";
 import { CompletedTasks } from "../entity/completedTaks";
 import { Tasks } from "../entity/tasks";
@@ -9,18 +10,23 @@ class completedController {
     const repositoryForVerify = getMongoRepository(Tasks);
 
     const { id } = req.params;
-    const { done } = req.body
+    const { done } = req.body;
 
     const verifTask = await repositoryForVerify.findOne(id);
-    
-    const markedTask = {...verifTask, done: done};
 
-    await saveCompletedTask(markedTask)
+    const markedTask = { ...verifTask, done: done };
+
+    await saveCompletedTask(markedTask);
 
     const taskToSave = repositoryForVerify.create(markedTask as any);
     const task = await repositoryForVerify.save(taskToSave);
 
     return res.status(201).json(task);
+  }
+  async readAllCompletedTasks(req: Request, res: Response) {
+    const repository = getMongoRepository(CompletedTasks);
+    const read = await repository.find()
+    return res.status(200).json(read)
   }
 }
 
