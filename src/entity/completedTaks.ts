ef9b3ed4d40f4f4fb4000c8f97b946bada53d545
devAndre-Isaac <andreisaac344@gmail.com>
2021-11-16 19:32:10 -0300

    style: :art: format

--- src/entity/completedTaks.ts
+++ src/entity/completedTaks.ts
@@ -1,30 +1,29 @@
 import {
-    Entity,
-    Column,
-    CreateDateColumn,
-    UpdateDateColumn,
-    ObjectIdColumn,
-    ObjectID,
-  } from "typeorm";
-  
-  @Entity("completed_tasks")
-  export class CompletedTasks {
-    @ObjectIdColumn()
-    id: ObjectID;
-  
-    @Column({nullable: true})
-    task: string
-    
-    @Column({nullable: false})
-    done: boolean
-    
-    @Column({nullable: true})
-    description: string;
+  Entity,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ObjectIdColumn,
+  ObjectID,
+} from "typeorm";
 
-    @CreateDateColumn()
-    created_at: Date;
-  
-    @UpdateDateColumn()
-    update_at: Date;
-  }
-  
+@Entity("completed_tasks")
+export class CompletedTasks {
+  @ObjectIdColumn()
+  id: ObjectID;
+
+  @Column({ nullable: true })
+  task: string;
+
+  @Column({ nullable: false })
+  done: boolean;
+
+  @Column({ nullable: true })
+  description: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  update_at: Date;
+}
