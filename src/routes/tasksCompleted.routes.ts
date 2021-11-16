e9ee757f847fd5c3752a78f7ebd756ce8905c396
devAndre-Isaac <andreisaac344@gmail.com>
2021-11-16 19:32:26 -0300

    feat: :sparkles: add route for getAll completed task

--- src/routes/tasksCompleted.routes.ts
+++ src/routes/tasksCompleted.routes.ts
@@ -5,5 +5,6 @@ import completedController from "../controller/completed";
 const routes = Router();
 
 routes.post("/completed/:id", completedController.mark)
+routes.get("/completed/readAll", completedController.readAllCompletedTasks)
 
 export default routes
