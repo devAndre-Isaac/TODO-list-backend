dbf1de3c7d1890519a67a67742b2dfcc9cb8e74b
devAndre-Isaac <andreisaac344@gmail.com>
2021-11-17 17:26:57 -0300

    feat: :sparkles: add readCompletedTaskById route

--- src/routes/tasksCompleted.routes.ts
+++ src/routes/tasksCompleted.routes.ts
@@ -6,5 +6,7 @@ const routes = Router();
 
 routes.post("/completed/:id", completedController.mark);
 routes.get("/completed/readAll", completedController.readAllCompletedTasks);
+routes.get("/completed/read/:id", completedController.readCompletedTaskById);
+
 
 export default routes;
