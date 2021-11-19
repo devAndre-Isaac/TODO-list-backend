3a7e464c34420cd8f359df67674246c15f9f678d
devAndre-Isaac <andreisaac344@gmail.com>
2021-11-19 19:15:38 -0300

    refactor: :recycle: change save completed task service

--- src/service/saveCompletedTask.ts
+++ src/service/saveCompletedTask.ts
@@ -1,15 +1,12 @@
 import { getMongoRepository, ObjectID } from "typeorm";
 import { CompletedTasks } from "../entity/completedTaks";
 
-export const saveCompletedTask = async (markedTask: {
+export const saveCompletedTask = async (upCreate: {
   done: any;
-  id?: ObjectID | undefined;
   task?: string | undefined;
   description?: string | undefined;
-  created_at?: Date | undefined;
-  update_at?: Date | undefined;
 }) => {
   const repositoryForVerify = getMongoRepository(CompletedTasks);
-  const create = repositoryForVerify.create(markedTask);
-  await repositoryForVerify.save(create);
+  const create = repositoryForVerify.create(upCreate);
+  return await repositoryForVerify.save(create);
 };
