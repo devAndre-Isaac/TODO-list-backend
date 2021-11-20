3c9d9e70830990472d50ec558fb1b5e90f285c80
devAndre-Isaac <andreisaac344@gmail.com>
2021-11-20 18:21:18 -0300

    chore: :label: update types in service

--- src/service/saveCompletedTask.ts
+++ src/service/saveCompletedTask.ts
@@ -1,12 +1,12 @@
-import { getMongoRepository, ObjectID } from "typeorm";
+import { getMongoRepository } from "typeorm";
 import { CompletedTasks } from "../entity/completedTaks";
 
-export const saveCompletedTask = async (upCreate: {
-  done: any;
-  task?: string | undefined;
-  description?: string | undefined;
-}) => {
+export const saveCompletedTask = async (
+  task: string,
+  description?: string,
+  done?: any,
+) => {
   const repositoryForVerify = getMongoRepository(CompletedTasks);
-  const create = repositoryForVerify.create(upCreate);
+  const create = repositoryForVerify.create({task, description, done});
   return await repositoryForVerify.save(create);
 };
